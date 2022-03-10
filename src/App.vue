88cfe8a62ac3bd3e771b8042a3593a0b8495c44b
coderken <jk0910948659@gmail.com>
2022-03-10 22:35:37 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div v-for="item in scenicSpot" :key="item.ScenicSpotID">
+      <img :src="item.Picture.PictureUrl1" :alt="item.Picture.PictureDescription1" />
+      <p>地址：{{ item.Address }}</p>
+      <p>開放時間：{{ item.Address }}</p>
+      <p>景點名稱：{{ item.ScenicSpotName }}</p>
+      <p>描述：{{ item.Description }}</p>
+      <p>詳細資訊：{{ item.Description }}</p>
+      <p>更新時間：{{ item.SrcUpdateTime }}</p>
+      <p>聯絡資訊：{{ item.Phone }}</p>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+
+<script>
+export default {
+  data() {
+    return {
+      listQuery: {
+        $top: 10,
+        $format: "JSON",
+      },
+      scenicSpot: [],
+    }
+  },
+  methods: {
+    getList() {
+      this.$api.scenicSpot.Load(this.listQuery).then((res) => {
+        console.log(JSON.parse(JSON.stringify(res)))
+        this.scenicSpot = res
+      })
+    },
+  },
+  mounted() {
+    this.getList()
+  },
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
