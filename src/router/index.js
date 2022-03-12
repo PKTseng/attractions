678619046aa36be915e2e0ebc50b276d2348d2f6
coderken <jk0910948659@gmail.com>
2022-03-12 18:13:42 +0800

    fix route

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,37 @@ import VueRouter from "vue-router"
 
 Vue.use(VueRouter)
 
-const scenicSpot = () => import("../views/scenicSpot")
+const home = () => import("../views/home")
+
+const scenicSpot = () => import("../views/scenicSpot.vue")
+const scenicSpotList = () => import("../views/scenicSpot/index")
+const scenicSpotDetail = () => import("../views/scenicSpot/detailInfo")
+
 const About = () => import("../views/About.vue")
 
 const routes = [
   {
     path: "/",
-    name: "scenicSpot",
-    component: scenicSpot,
+    name: "home",
+    component: home,
   },
+  {
+    path: "/scenicSpot",
+    component: scenicSpot,
+    children: [
+      {
+        path: "list",
+        name: "scenicSpotList",
+        component: scenicSpotList,
+      },
+      {
+        path: "detail/:id",
+        name: "scenicSpotDetail",
+        component: scenicSpotDetail,
+      },
+    ],
+  },
+
   {
     path: "/about",
     name: "About",
